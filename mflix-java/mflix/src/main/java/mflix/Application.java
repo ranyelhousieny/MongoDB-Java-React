02647ec684b608612c78fb2420ba09c1ae68f4d0
Rany <ranyel@msn.com>
2020-04-03 12:12:14 -0700

    3 Apr

--- mflix-java/mflix/src/main/java/mflix/Application.java
+++ mflix-java/mflix/src/main/java/mflix/Application.java
@@ -27,7 +27,8 @@ public class Application {
                         + "   \\ | /\n"
                         + "    \\|/\n"
                         + "     |\n"
-                        + "                       \n";
+                        + "                       \n" +
+                        "Rany ElHousieny";
         System.out.println(welcomeMessage);
     }
 }
